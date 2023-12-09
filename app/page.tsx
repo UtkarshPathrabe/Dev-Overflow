691f2c907ee44c8afff4a013770e8907c04dab3a
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-12-09 21:40:18 +0530

    Delete page.tsx

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">NextJS 13 is here!</h1>
-      <h2 className="h2-bold">NextJS 13 is here!</h2>
-      <h3 className="h3-bold">NextJS 13 is here!</h3>
-    </div>
-  );
-}
