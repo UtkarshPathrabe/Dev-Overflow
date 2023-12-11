94095d083417b5181aa04f73838d02aa1f0e67f8
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-12-11 20:34:38 +0530

    Update page.tsx

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,3 +1,4 @@
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
@@ -13,7 +14,14 @@ export default function Home() {
         </Link>
       </div>
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-        LocalSearchbar Filters
+        <LocalSearchbar
+          route="/"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search questions..."
+          otherClasses="flex-1"
+        />{" "}
+        Filter
       </div>
     </>
   );
