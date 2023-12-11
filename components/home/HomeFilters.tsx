926b29c803dc11003891837d314af6093683a91e
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-12-11 21:20:24 +0530

    Update HomeFilters.tsx

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -13,7 +13,7 @@ const HomeFilters = () => {
           onClick={() => {}}
           className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
             active === item.value
-              ? "bg-primary-100 text-primary-500"
+              ? "bg-primary-100 text-primary-500 hover:bg-light-800 dark:hover:bg-dark-300"
               : "bg-light-800 text-light-500 hover:bg-light-700 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-400"
           }`}>
           {item.name}
