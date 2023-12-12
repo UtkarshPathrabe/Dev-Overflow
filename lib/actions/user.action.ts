099c06bd14109b7178dadc661672ea5551e5dee7
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-12-12 21:16:34 +0530

    Update user.action.ts

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -35,9 +35,7 @@ export async function getUserById(params: any) {
 export async function createUser(userData: CreateUserParams) {
   try {
     connectToDatabase();
-
     const newUser = await User.create(userData);
-
     return newUser;
   } catch (error) {
     console.log(error);
