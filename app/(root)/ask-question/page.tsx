d9d9c2c71601f80a8cff1a7b6c49b37beb77d40a
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-12-12 16:16:16 +0530

    Update page.tsx

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,14 +1,26 @@
 import Question from "@/components/forms/Question";
+import { getUserById } from "@/lib/actions/user.action";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import React from "react";
 
-const AskQuestion = () => {
+const Page = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const mongoUser = await getUserById({ userId });
+
   return (
-    <div className="flex flex-col">
+    <div>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
       <div className="mt-9">
-        <Question mongoUserId="" />
+        <Question mongoUserId={JSON.stringify(mongoUser?._id)} />
       </div>
     </div>
   );
 };
 
-export default AskQuestion;
+export default Page;
