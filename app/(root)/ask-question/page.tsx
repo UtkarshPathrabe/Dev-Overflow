d4d91eb619d4eedf169e4c2b43bce146906396a5
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-12-12 12:24:38 +0530

    Update page.tsx

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,5 +1,14 @@
+import Question from "@/components/forms/Question";
+
 const AskQuestion = () => {
-  return <div>Ask Question</div>;
+  return (
+    <div className="flex flex-col">
+      <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
+  );
 };
 
 export default AskQuestion;
