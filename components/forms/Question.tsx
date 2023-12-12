19ca6564aadc86604cacdd482754b61a05958933
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-12-12 16:16:18 +0530

    Update Question.tsx

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -21,7 +21,8 @@ import { QuestionSchema } from "@/lib/validations";
 import { useTheme } from "@/context/ThemeProvider";
 import { Badge } from "../ui/badge";
 import Image from "next/image";
-// import { usePathname, useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { createQuestion, editQuestion } from "@/lib/actions/question.action";
 
 interface Props {
   type?: string;
@@ -33,8 +34,8 @@ export function Question({ type, mongoUserId, questionDetails }: Props) {
   const { mode } = useTheme();
   const editorRef = useRef(null);
   const [isSubmitting, setIsSubmitting] = useState(false);
-  // const router = useRouter();
-  // const pathname = usePathname();
+  const router = useRouter();
+  const pathname = usePathname();
 
   const parsedQuestionDetails =
     questionDetails && JSON.parse(questionDetails || "");
@@ -55,24 +56,24 @@ export function Question({ type, mongoUserId, questionDetails }: Props) {
   async function onSubmit(values: z.infer<typeof QuestionSchema>) {
     setIsSubmitting(true);
     try {
-      // if(type === 'Edit') {
-      //   await editQuestion({
-      //     questionId: parsedQuestionDetails._id,
-      //     title: values.title,
-      //     content: values.explanation,
-      //     path: pathname,
-      //   })
-      //   router.push(`/question/${parsedQuestionDetails._id}`);
-      // } else {
-      //   await createQuestion({
-      //     title: values.title,
-      //     content: values.explanation,
-      //     tags: values.tags,
-      //     author: JSON.parse(mongoUserId),
-      //     path: pathname,
-      //   });
-      //   router.push('/');
-      // }
+      if (type === "Edit") {
+        await editQuestion({
+          questionId: parsedQuestionDetails._id,
+          title: values.title,
+          content: values.explanation,
+          path: pathname,
+        });
+        router.push(`/question/${parsedQuestionDetails._id}`);
+      } else {
+        await createQuestion({
+          title: values.title,
+          content: values.explanation,
+          tags: values.tags,
+          author: JSON.parse(mongoUserId),
+          path: pathname,
+        });
+        router.push("/");
+      }
     } catch (error) {
     } finally {
       setIsSubmitting(false);
