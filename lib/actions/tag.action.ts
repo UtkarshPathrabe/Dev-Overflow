2cc1c8076b69c61caa4a1cb703d6f5e295d84d35
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-12-13 13:17:01 +0530

    Update tag.action.ts

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -14,16 +14,11 @@ import { FilterQuery } from "mongoose";
 export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
   try {
     connectToDatabase();
-
     const { userId } = params;
-
     const user = await User.findById(userId);
-
     if (!user) throw new Error("User not found");
-
     // Find interactions for the user and group by tags...
     // Interaction...
-
     return [
       { _id: "1", name: "tag" },
       { _id: "2", name: "tag2" },
