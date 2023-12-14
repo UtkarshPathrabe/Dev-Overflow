6c8b3a78624a6b039c5245f348d13a0fb36cc23f
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-12-14 14:51:48 +0530

    Update user.action.ts

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -155,17 +155,12 @@ export async function toggleSaveQuestion(params: ToggleSaveQuestionParams) {
 export async function getSavedQuestions(params: GetSavedQuestionsParams) {
   try {
     connectToDatabase();
-
     const { clerkId, searchQuery, filter, page = 1, pageSize = 20 } = params;
-
     const skipAmount = (page - 1) * pageSize;
-
     const query: FilterQuery<typeof Question> = searchQuery
       ? { title: { $regex: new RegExp(searchQuery, "i") } }
       : {};
-
     let sortOptions = {};
-
     switch (filter) {
       case "most_recent":
         sortOptions = { createdAt: -1 };
@@ -182,11 +177,9 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
       case "most_answered":
         sortOptions = { answers: -1 };
         break;
-
       default:
         break;
     }
-
     const user = await User.findOne({ clerkId }).populate({
       path: "saved",
       match: query,
@@ -200,15 +193,11 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
         { path: "author", model: User, select: "_id clerkId name picture" },
       ],
     });
-
     const isNext = user.saved.length > pageSize;
-
     if (!user) {
       throw new Error("User not found");
     }
-
     const savedQuestions = user.saved;
-
     return { questions: savedQuestions, isNext };
   } catch (error) {
     console.log(error);
