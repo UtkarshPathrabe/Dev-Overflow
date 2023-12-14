6cc4924680d69d8635a0f2dd7c76c0b6a04d1540
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-12-14 21:51:22 +0530

    Update interaction.action.tsx

--- lib/actions/interaction.action.tsx
+++ lib/actions/interaction.action.tsx
@@ -8,21 +8,18 @@ import Interaction from "@/database/interaction.model";
 export async function viewQuestion(params: ViewQuestionParams) {
   try {
     await connectToDatabase();
-
     const { questionId, userId } = params;
-
     // Update view count for the question
     await Question.findByIdAndUpdate(questionId, { $inc: { views: 1 } });
-
     if (userId) {
       const existingInteraction = await Interaction.findOne({
         user: userId,
         action: "view",
         question: questionId,
       });
-
-      if (existingInteraction) return console.log("User has already viewed.");
-
+      if (existingInteraction) {
+        return console.log("User has already viewed.");
+      }
       // Create interaction
       await Interaction.create({
         user: userId,
