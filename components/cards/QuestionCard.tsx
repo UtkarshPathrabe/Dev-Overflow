20d5db819c96281a0d5ffdf1941c802ff85d6d2d
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-12-15 19:20:04 +0530

    Update QuestionCard.tsx

--- components/cards/QuestionCard.tsx
+++ components/cards/QuestionCard.tsx
@@ -38,7 +38,7 @@ const QuestionCard = ({
 
   return (
     <div className="card-wrapper rounded-[10px] p-9 sm:px-11">
-      <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
+      <div className="flex flex-col-reverse items-center justify-between gap-5 sm:flex-row">
         <div>
           <span className="subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden">
             {getTimestamp(createdAt)}
