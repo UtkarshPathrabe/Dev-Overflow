107e1a99f9c45a81602a944c8014b9047cf9235c
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-12-15 10:47:16 +0530

    Update answer.model.ts

--- database/answer.model.ts
+++ database/answer.model.ts
@@ -10,36 +10,12 @@ export interface IAnswer extends Document {
 }
 
 const AnswerSchema = new Schema({
-  author: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  question: {
-    type: Schema.Types.ObjectId,
-    ref: "Question",
-    required: true,
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  upvotes: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-  downvotes: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-    },
-  ],
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  question: { type: Schema.Types.ObjectId, ref: "Question", required: true },
+  content: { type: String, required: true },
+  upvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  downvotes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  createdAt: { type: Date, default: Date.now },
 });
 
 const Answer = models.Answer || model("Answer", AnswerSchema);
