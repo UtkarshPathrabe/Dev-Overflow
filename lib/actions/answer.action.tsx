76a642780619e51fcbee5460f144cf4efa6a1c7b
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-12-15 10:47:18 +0530

    Update answer.action.tsx

--- lib/actions/answer.action.tsx
+++ lib/actions/answer.action.tsx
@@ -149,22 +149,17 @@ export async function downvoteAnswer(params: AnswerVoteParams) {
 export async function deleteAnswer(params: DeleteAnswerParams) {
   try {
     connectToDatabase();
-
     const { answerId, path } = params;
-
     const answer = await Answer.findById(answerId);
-
     if (!answer) {
       throw new Error("Answer not found");
     }
-
     await answer.deleteOne({ _id: answerId });
     await Question.updateMany(
       { _id: answer.question },
       { $pull: { answers: answerId } }
     );
     await Interaction.deleteMany({ answer: answerId });
-
     revalidatePath(path);
   } catch (error) {
     console.log(error);
