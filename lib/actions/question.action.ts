747aa2974951cf018dae562c731afd2e9b44a51e
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-12-16 22:16:34 +0530

    Update question.action.ts

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -207,20 +207,14 @@ export async function deleteQuestion(params: DeleteQuestionParams) {
 export async function editQuestion(params: EditQuestionParams) {
   try {
     connectToDatabase();
-
     const { questionId, title, content, path } = params;
-
     const question = await Question.findById(questionId).populate("tags");
-
     if (!question) {
       throw new Error("Question not found");
     }
-
     question.title = title;
     question.content = content;
-
     await question.save();
-
     revalidatePath(path);
   } catch (error) {
     console.log(error);
