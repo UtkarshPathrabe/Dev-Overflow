f2b40d8f43089a49ddf403bad0068efd90387d72
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-12-16 22:16:20 +0530

    Update EditDeleteAction.tsx

--- components/shared/EditDeleteAction.tsx
+++ components/shared/EditDeleteAction.tsx
@@ -5,6 +5,23 @@ import { deleteQuestion } from "@/lib/actions/question.action";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { useCallback } from "react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 interface Props {
   type: string;
@@ -38,23 +55,64 @@ const EditDeleteAction = ({ type, itemId }: Props) => {
   return (
     <div className="flex items-center justify-end gap-3 max-sm:w-full">
       {type === "Question" && (
-        <Image
-          src="/assets/icons/edit.svg"
-          alt="Edit"
-          width={14}
-          height={14}
-          className="cursor-pointer object-contain"
-          onClick={handleEdit}
-        />
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Image
+                src="/assets/icons/edit.svg"
+                alt="Edit"
+                width={14}
+                height={14}
+                className="cursor-pointer object-contain"
+                onClick={handleEdit}
+              />
+            </TooltipTrigger>
+            <TooltipContent className="text-dark300_light700">
+              Edit {type}
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       )}
-      <Image
-        src="/assets/icons/trash.svg"
-        alt="Delete"
-        width={14}
-        height={14}
-        className="cursor-pointer object-contain"
-        onClick={handleDelete}
-      />
+      <AlertDialog>
+        <AlertDialogTrigger>
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Image
+                  src="/assets/icons/trash.svg"
+                  alt="Delete"
+                  width={14}
+                  height={14}
+                  className="cursor-pointer object-contain"
+                />
+              </TooltipTrigger>
+              <TooltipContent className="text-dark300_light700">
+                Delete {type}
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </AlertDialogTrigger>
+        <AlertDialogContent className="background-light800_dark300 text-dark300_light700">
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete this{" "}
+              {type === "Question"
+                ? "question and corresponding answers"
+                : "answer"}{" "}
+              from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel className="border-2">Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={handleDelete}
+              className="w-fit bg-red-500 !text-light-900">
+              Continue
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </div>
   );
 };
