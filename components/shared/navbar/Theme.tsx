164624c256ae778b8769cd33118051b611235b1c
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-12-16 22:16:24 +0530

    Update Theme.tsx

--- components/shared/navbar/Theme.tsx
+++ components/shared/navbar/Theme.tsx
@@ -51,7 +51,7 @@ const Theme = () => {
             />
           )}
         </MenubarTrigger>
-        <MenubarContent className="absolute right-[-3rem] mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300">
+        <MenubarContent className="background-light800_dark300 text-dark300_light700 absolute right-[-3rem] mt-3 min-w-[120px] rounded border py-2 dark:border-dark-400 dark:bg-dark-300">
           {themes.map((item) => (
             <MenubarItem
               key={item.value}
