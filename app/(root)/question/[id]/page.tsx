d84759884b2a938819a3e85323392f43d526b7a1
Utkarsh Pathrabe <uapathrabe@gmail.com>
2023-12-19 12:43:14 +0530

    Update page.tsx

--- app/(root)/question/[id]/page.tsx
+++ app/(root)/question/[id]/page.tsx
@@ -47,9 +47,9 @@ const Page = async ({ params, searchParams }: any) => {
               itemId={JSON.stringify(result._id)}
               userId={JSON.stringify(mongoUser?._id)}
               upvotes={result.upvotes.length}
-              hasupVoted={result.upvotes.includes(mongoUser?._id)}
+              hasAlreadyUpvoted={result.upvotes.includes(mongoUser?._id)}
               downvotes={result.downvotes.length}
-              hasdownVoted={result.downvotes.includes(mongoUser?._id)}
+              hasAlreadyDownvoted={result.downvotes.includes(mongoUser?._id)}
               hasSaved={mongoUser?.saved.includes(result._id)}
             />
           </div>
