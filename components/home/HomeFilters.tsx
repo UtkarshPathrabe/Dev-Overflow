3514cbaefc60dce0cbd1ea8a94b18ee09fec8f46
Utkarsh Ashok Pathrabe <uapathrabe@gmail.com>
2023-12-20 14:23:44 +0530

    Update HomeFilters.tsx

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -47,7 +47,8 @@ const HomeFilters = () => {
             active === item.value
               ? "bg-primary-100 text-primary-500 hover:bg-light-800 dark:hover:bg-dark-300"
               : "bg-light-800 text-light-500 hover:bg-light-700 dark:bg-dark-300 dark:text-light-500 dark:hover:bg-dark-400"
-          }`}>
+          }`}
+        >
           {item.name}
         </Button>
       ))}
