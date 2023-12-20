ad9e1f5b7a6d51085a6d184564a329cbc846c306
Utkarsh Ashok Pathrabe <uapathrabe@gmail.com>
2023-12-20 11:34:35 +0530

    Update page.tsx

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,4 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign Up | Dev Overflow",
+};
 
 export default function Page() {
   return <SignUp />;
