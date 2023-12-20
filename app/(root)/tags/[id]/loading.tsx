4a5bd2becb882ae75143b2b46c036374aecc752a
Utkarsh Ashok Pathrabe <uapathrabe@gmail.com>
2023-12-20 11:35:00 +0530

    Update loading.tsx

--- app/(root)/tags/[id]/loading.tsx
+++ app/(root)/tags/[id]/loading.tsx
@@ -7,9 +7,12 @@ const Loading = () => {
 
       <Skeleton className="mb-12 mt-11 h-14 w-full" />
 
-      <div className="mt-10 flex flex-col gap-6">
+      <div className="mt-10 flex w-full flex-col gap-6">
         {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
-          <Skeleton key={item} className="h-48 w-full rounded-xl" />
+          <Skeleton
+            key={item}
+            className="card-wrapper h-48 w-full rounded-[10px] p-9 sm:px-11"
+          />
         ))}
       </div>
     </section>
