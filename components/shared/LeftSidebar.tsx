d696236512ea201cf42fc401fb32849688024ab1
Utkarsh Ashok Pathrabe <uapathrabe@gmail.com>
2023-12-20 21:31:53 +0530

    Update LeftSidebar.tsx

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -11,7 +11,7 @@ const LeftSidebar = () => {
   const { userId } = useAuth();
   const pathname = usePathname();
   return (
-    <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between overflow-y-auto border-r p-6 pt-36 shadow-light-300 dark:shadow-none max-sm:hidden lg:w-[266px]">
+    <section className="background-light900_dark200 light-border custom-scrollbar sticky left-0 top-0 flex h-screen flex-col justify-between gap-6 overflow-y-auto border-r p-6 pt-36 shadow-light-300 max-sm:hidden lg:w-[266px] dark:shadow-none">
       <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map((item) => {
           const isActive =
@@ -32,7 +32,8 @@ const LeftSidebar = () => {
                 isActive
                   ? "primary-gradient rounded-lg text-light-900"
                   : "text-dark300_light900"
-              } flex items-center justify-start gap-4 bg-transparent p-4`}>
+              } flex items-center justify-start gap-4 bg-transparent p-4`}
+            >
               <Image
                 src={item.imgURL}
                 alt={item.label}
@@ -43,7 +44,8 @@ const LeftSidebar = () => {
               <p
                 className={`${
                   isActive ? "base-bold" : "base-medium"
-                } max-lg:hidden`}>
+                } max-lg:hidden`}
+              >
                 {item.label}
               </p>
             </Link>
