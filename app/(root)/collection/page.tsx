de1802d6625d5725d50453863c1ac5f2ee0dd3ee
Utkarsh Ashok Pathrabe <uapathrabe@gmail.com>
2023-12-20 11:34:42 +0530

    Update page.tsx

--- app/(root)/collection/page.tsx
+++ app/(root)/collection/page.tsx
@@ -12,6 +12,11 @@ import { QuestionFilters } from "@/constants/filters";
 import { getSavedQuestions } from "@/lib/actions/user.action";
 import { SearchParamsProps } from "@/types";
 import { auth } from "@clerk/nextjs";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Collection | Dev Overflow",
+};
 
 export default async function Home({ searchParams }: SearchParamsProps) {
   const { userId } = auth();
