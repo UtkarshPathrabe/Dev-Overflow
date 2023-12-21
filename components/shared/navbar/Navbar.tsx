f2ee7116aace204d127541e2641afc715d7dd304
Utkarsh Ashok Pathrabe <uapathrabe@gmail.com>
2023-12-21 10:39:23 +0530

    Update Navbar.tsx

--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -8,7 +8,7 @@ import GlobalSearch from "../search/GlobalSearch";
 
 const Navbar = () => {
   return (
-    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 sm:px-12 dark:shadow-none">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
@@ -16,7 +16,7 @@ const Navbar = () => {
           height={23}
           alt="DevFlow"
         />
-        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
+        <p className="h2-bold font-spaceGrotesk text-dark-100 max-sm:hidden dark:text-light-900">
           Dev<span className="text-primary-500">Overflow</span>
         </p>
       </Link>
@@ -25,7 +25,7 @@ const Navbar = () => {
         <Theme />
         <SignedIn>
           <UserButton
-            afterSignOutUrl="/"
+            afterSignOutUrl="/sign-in"
             appearance={{
               elements: {
                 avatarBox: "h-10 w-10",
