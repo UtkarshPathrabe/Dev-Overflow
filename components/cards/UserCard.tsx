36606b5cc84156e50568212916f1bb476e0d73d9
Utkarsh Ashok Pathrabe <uapathrabe@gmail.com>
2023-12-21 10:39:21 +0530

    Update UserCard.tsx

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -21,7 +21,8 @@ const UserCard = async ({ user }: Props) => {
   return (
     <Link
       href={`/profile/${user.clerkId}`}
-      className="shadow-light100_darknone w-full max-xs:min-w-full xs:w-[260px]">
+      className="shadow-light100_darknone w-full max-xs:min-w-full xs:w-[260px]"
+    >
       <article className="background-light900_dark200 light-border flex w-full flex-col items-center justify-center rounded-2xl border p-8">
         <Image
           src={user.picture}
@@ -48,7 +49,9 @@ const UserCard = async ({ user }: Props) => {
               ))}
             </div>
           ) : (
-            <Badge>No tags yet</Badge>
+            <Badge className="small-regular background-light800_dark300 text-dark200_light900 rounded-md border-none px-4 py-2">
+              No tags yet
+            </Badge>
           )}
         </div>
       </article>
